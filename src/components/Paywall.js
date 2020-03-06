681e2282f4ab0a813a0cc764a181375d889693d7
CoderGuy614 <jlutz.110@gmail.com>
2020-03-06 17:00:01 +0800

    Stripe Payment partially finished

--- src/components/Paywall.js
+++ src/components/Paywall.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import {} from 'react-stripe-elements'
+import React from "react";
+import { StripeProvider, Elements } from "react-stripe-elements";
 
-import '../styles/paywall.css'
-
-import Stripe from './Stripe'
+import "../styles/paywall.css";
+import Stripe from "./Stripe";
 
 class Paywall extends React.Component {
-	render() {
-		return (
-			<div id="paywall" className={this.props.paywallOpen ? 'open' : ''}>
-				<div id="card">
-					<i
-						id="close"
-						className="fas fa-fw fa-times"
-						onClick={() => this.props.closePaywall()}
-					></i>
-					<h2>Upgrade to Premium</h2>
-					{/* stripe */}
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div id="paywall" className={this.props.paywallOpen ? "open" : ""}>
+        <div id="card">
+          <i
+            id="close"
+            className="fas fa-fw fa-times"
+            onClick={() => this.props.closePaywall()}
+          ></i>
+          <h2>Upgrade to Premium</h2>
+          <StripeProvider apiKey="pk_test_Lujy9JAzF8Ate9R5xFZiJT4X007sudUFX4">
+            <Elements>
+              <Stripe />
+            </Elements>
+          </StripeProvider>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Paywall
+export default Paywall;
